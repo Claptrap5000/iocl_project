786e787e64fdc87dff32e2f2c4c8e8f6506d8020
Jintus <jintusharma56@gmail.com>
2022-07-11 01:01:33 +0530

    Added Sesction from Database Data

--- item_allotment/item.php
+++ item_allotment/item.php
@@ -1,31 +1,71 @@
 <?php
-include_once '../db.php';
-if(isset($_POST['submit']))
-{    
-    $wno = $_POST['wno'];
-    $item_no = $_POST['item_no'];
-    $item_desc = $_POST['item_desc'];
-    $item_qty= $_POST['item_qty'];
-    $item_unit= $_POST['item_unit'];
-   
-// wno	item_no	item_desc	item_qty	item_unit
-     $sql = "insert into item_allot (wno,item_no,item_desc,item_qty,item_unit) values('$wno','$item_no','$item_desc','$item_qty','$item_unit')";
-     
-     if (mysqli_query($con, $sql)) {
-      //   echo "New record has been added successfully !";
-        echo '<script>
-               location.replace("item.html");
-               alert("New record has been added successfully ");
-            </script>';
-     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($con);
-        echo '<script>
-               location.replace("item.html");
-               alert("Error foun in Your Entry!");
-            </script>';
-     }
-     mysqli_close($con);
-}
+    include_once '../db.php';
+    // include_once '../style/style.css';
+    $sql_wno = 'SELECT * FROM workallot';
+    $all_categories = mysqli_query($con, $sql_wno);
+                            
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Item Allotment</title>
+</head>
+<body>
+    <header>
+        <h1>Item Allotment</h1>
+    </header>
+    
+<!-- wno	item_no	item_desc	item_qty	item_unit -->
+    <section>
+        <div class="option">
+            <form action="item_2.php" method="post">
+            <label>Select Work ID</label>
+            <div class="selection">
+                <select name="wno" id="sel_wno">
+                        <?php
+                            while ($category = mysqli_fetch_array(
+                                $all_categories,MYSQLI_ASSOC)):; 
+                        ?>
 
+                        <option value="<?php echo $category["wno"];?>">     <!-- Here it is Primary Key -->
+                                <?php echo $category["wno"];?>              <!-- Here it is Show Key -->
+                        </option>
+                        <?php endwhile; ?>
+                </select>
+                    <!-- <input type="text" name="wno" placeholder="WORK NO."> -->
+            </div>
+                <input type="text" name="item_no" placeholder="ITEM NO.">
+                <input type="text" name="item_desc" placeholder="DESC">
+                <input type="text" name="item_qty" placeholder="QUANTITY">
+                <input type="text" name="item_unit" placeholder="UNIT">
+                <button type="submit" name="submit">SUBMIT</button>
+                <!-- need to change name attr -->
+            </form>
+            <!-- <a href="#"><button id="r_cd">Register as Contractor</button></a> -->
+        </div>
+    </section>
+</body>
+<style>
+    
+    form{
+        padding: 4.5em 3em 1em 3em;
+    }
+    label{
+        background-color: white;
+        margin: auto;
+    }
+    .selection{
+        background-color: white;
+    }
+    #sel_wno{
+        padding: 2em 15em;
+        margin-bottom :1em;
+        background-color: white;
+    }
+</style>
+</html>
--- work_allotment/work_order.php
+++ work_allotment/work_order.php
@@ -1,31 +1,96 @@
 <?php
-include_once '../db.php';
-if(isset($_POST['submit']))
-{    
-    $wno = $_POST['wno'];
-    $empno = $_POST['empno'];
-    $vcode = $_POST['vcode'];
-    $des= $_POST['des'];
-    $entdate= $_POST['entdate'];
-
-    $sql = "insert into workallot (wno,empno,vcode,des,entdate) values('$wno','$empno','$vcode','$des','$entdate')";
-     
-   
-   if (mysqli_query($con, $sql)) 
-   {
-      echo '<script>
-               location.replace("work_order.html");
-               alert("New record has been added successfully ");
-            </script>';
-     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($con);
-        echo '<script>
-               location.replace("work_order.html");
-               alert("Error foun in Your Entry!");
-            </script>';
-     }
-     mysqli_close($con);
-}
+    include_once '../db.php';
+    // include_once '../style/style.css';
+    $sql_emp_no = 'SELECT * FROM employee';
+    $sql_vcode = 'SELECT * FROM vendor';
+    $all_categories = mysqli_query($con, $sql_emp_no);
+    $new = mysqli_query($con, $sql_vcode);
+                            
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Work Allotment</title>
+</head>
+<body>
+    <header>
+        <h1>Work Allotment</h1>
+    </header>
+    <!-- wno	empno	vcode	des	entdate -->
+    <section>
+        <div class="option">
+            <form action="work_order2.php" method="post">
+                <input type="text" name="wno" placeholder="Enter Your Work ID">
 
+                <label>Select Your Employee ID</label>
+            <div class="selection">
+                <select name="empno" id="sel_wno">
+                        <?php
+                            while ($category = mysqli_fetch_array(
+                                $all_categories,MYSQLI_ASSOC)):; 
+                        ?>
+
+                        <option value="<?php echo $category["empno"];?>">     <!-- Here it is Primary Key -->
+                                <?php echo $category["empno"];?>              <!-- Here it is Show Key -->
+                        </option>
+                        <?php endwhile; ?>
+                </select>
+                    <!-- <input type="text" name="empno" placeholder="Enter Your Emp No"> -->
+            </div>
+            
+                <label>Select Your Vendor Code</label>
+            <div class="selection">
+                    <select name="vcode" id="sel_ino">
+                        <?php
+                            while ($category1 = mysqli_fetch_array(
+                                $new,MYSQLI_ASSOC)):; 
+                        ?>
+
+                        <option value="<?php echo $category1["vcode"];?>">
+                                <?php echo $category1["vcode"];?>
+                        </option>
+                        <?php endwhile; ?>
+                    </select>
+                    <!-- <input type="text" name="vcode" placeholder="Enter Your Vendor Code"> -->
+                </div>
+                <input type="text" name="des" placeholder="Enter Your Description">
+                <!-- <input type="date" name="entdate" id="" placeholder="Enter Entry Date"> -->
+                <input class="textbox-n" type="text" onfocus="(this.type = 'date')" name="entdate" placeholder="Enter Entry Date">
+                <button type="submit" name="submit">SUBMIT</button>
+                <!-- need to change name attr -->
+            </form>
+            <!-- <a href="#"><button id="r_cd">Register as Contractor</button></a> -->
+        </div>
+    </section>
+</body>
+<style>
+    
+    form{
+        padding: 3em 2em 1em 2em;
+    }
+    #sel_wno{
+        padding: 2em 16em;
+        margin-bottom :1em;
+        background-color: white;
+    }
+    #sel_ino{
+        padding: 2em 15em;
+        margin-bottom :1em;
+        background-color: white;
+    }
+    label{
+        background-color: white;
+        margin: auto;
+    }
+    .selection{
+        background-color: white;
+    }
+</style>
+<!-- CREATE TABLE workallot( wno varchar(25), empno varchar(25), vcode varchar(25), des varchar(255), PRIMARY KEY (wno), FOREIGN KEY (empno) REFERENCES employee(empno), FOREIGN KEY (vcode) REFERENCES vendor(vcode) ); -->
+
+</html>
