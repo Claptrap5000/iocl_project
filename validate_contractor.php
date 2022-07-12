8915d7fde7f83b5a4467790f500e8255164bd5c1
Jintus <jintusharma56@gmail.com>
2022-07-12 17:24:44 +0530

    Minor Changes in Some Files

--- validate_contractor.php
+++ validate_contractor.php
@@ -1,25 +1,6 @@
 <?php
 
-// $servername="localhost";
-// $username="root";
-// $password="";
-// $dbname="keyboard";
-
-// $conn=new mysqli($servername,$username,$password,$dbname);
-
-// mysqli_select_db($con, 'iocl');
-
-// $wno = $_POST['wno'];
-// $item_no = $_POST['item_no'];
-// $item_desc = $_POST['item_desc'];
-// $qty= $_POST['qty'];
-// $unit= $_POST['unit'];
-
-// $reg = "insert into 'item' ('wno' , 'item_no', 'item_desc' , 'qty' , 'unit') values('$wno','$item_no','$item_desc','$qty','$unit')" ;
-// mysqli_query($con, $reg);
-// echo "data added";
 include 'db.php';
-
 
 $empno = $_POST['empno'];
 $emppassword = $_POST['emppassword'];
